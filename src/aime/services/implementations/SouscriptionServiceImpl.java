23c5492bf5348f3a5f22d7c6d8a8eede2a6cf767
marcapedoh <adralabel@gmail.com>
2024-04-29 11:21:24 +0200

    correct

--- src/aime/services/implementations/SouscriptionServiceImpl.java
+++ src/aime/services/implementations/SouscriptionServiceImpl.java
@@ -5,7 +5,9 @@
 package aime.services.implementations;
 
 import aime.entities.Sms;
+import aime.entities.Souscription;
 import aime.services.SmsService;
+import aime.services.SouscriptionService;
 import aime.utils.Connexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -19,65 +21,35 @@ import java.util.logging.Logger;
  *
  * @author DELL
  */
-public class SmsServiceImpl implements SmsService {
+public class SouscriptionServiceImpl implements SouscriptionService {
 
-    @Override
-    public void ajouterSms(Sms sms) {
-        Connection conn = null;
-        try {
-            conn = Connexion.getConnection();
-        } catch (SQLException ex) {
-            Logger.getLogger(SmsServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        PreparedStatement ps = null;
-
-        try {
-            String query = "INSERT INTO Sms (idClient, libelle) VALUES (?, ?)";
-            ps = conn.prepareStatement(query);
-            ps.setInt(1, sms.getIdClient());
-            ps.setString(2, sms.getLibelle());
-
-            ps.executeUpdate();
-            System.out.println("SMS ajouté avec succès !");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            Connexion.close(conn, ps, null);
-        }
-    }
-
-    @Override
-    public Sms trouverSmsParId(int id) {
-        Connection conn;
-        try {
-            conn = Connexion.getConnection();
-        } catch (SQLException ex) {
-            Logger.getLogger(SmsServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        Sms sms = null;
-
-        try {
-            String query = "SELECT * FROM Sms WHERE id = ?";
-            ps = conn.prepareStatement(query);
-            ps.setInt(1, id);
-            rs = ps.executeQuery();
-
-            if (rs.next()) {
-                sms = new Sms();
-                sms.setId(rs.getInt("id"));
-                sms.setIdClient(rs.getInt("idClient"));
-                sms.setLibelle(rs.getString("libelle"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            Connexion.close(conn, ps, rs);
-        }
-
-        return sms;
-    }
+   
+   
 
     // Autres méthodes CRUD à implémenter (mettreAJourSms, supprimerSms, obtenirTousLesSms, etc.)
+
+    @Override
+    public void ajouterSouscription(Souscription souscription) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Souscription trouverSouscriptionParId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mettreAJourSouscription(Souscription souscription) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void supprimerSouscription(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Souscription> obtenirToutesLesSouscriptions() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
